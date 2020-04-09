75859a6e6668b8168088c8b555da6e07b33bd26a
antonmiazyn <antonmiazyn@mail.ru>
2020-04-09 23:37:00 +0300

    page switcher

---
+++ projects/longShortCFD/assets/js/accordeon.js
@@ -0,0 +1,22 @@
+function accordeon() {
+  let link = document.querySelectorAll(".faq_accordeon-link");
+  let turn = document.querySelectorAll(".faq_accordeon-turn");
+  let content = document.querySelectorAll(".faq_accordeon-content");
+
+  for(let i = 0; i < link.length; i++) {
+    link[i].onclick = () => {
+      link.forEach((item, i) => {
+        item.classList.add("hidden");
+      });
+      content[i].classList.remove("hidden");
+    }
+    turn[i].onclick = () => {
+      content.forEach((item, i) => {
+        item.classList.add("hidden");
+      });
+      link.forEach((item, i) => {
+        item.classList.remove("hidden");
+      });
+    }
+  }
+}
---
+++ projects/longShortCFD/assets/js/page.js
@@ -0,0 +1,27 @@
+function page() {
+  let section = document.querySelectorAll(".main-page-section");
+  let prew = document.querySelectorAll(".menu__switcher_arrow_top");
+  let next = document.querySelectorAll(".btn__circle");
+
+  section.forEach((item, i) => {
+    item.classList.add("hidden");
+  });
+
+  section[0].classList.remove("hidden");
+
+  for(let i = 0; i < section.length; i++) {
+    next[i].onclick = () => {
+      section.forEach((item, i) => {
+        item.classList.add("hidden");
+      });
+      section[i+1].classList.remove("hidden");
+    }
+    prew[i].onclick = () => {
+      section.forEach((item, i) => {
+        item.classList.add("hidden");
+      });
+      section[i].classList.remove("hidden");
+    }
+  }
+
+}
