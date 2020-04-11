6c7d147ffcec698c84794a3d0adf9d216ad87863
antonmiazyn <antonmiazyn@mail.ru>
2020-04-11 19:01:35 +0300

    personal manager

--- projects/longShortCFD/assets/js/modal.js
+++ projects/longShortCFD/assets/js/modal.js
@@ -1,6 +1,7 @@
 let loginForm = document.querySelector(".login");
 let loginButton = document.querySelectorAll(".header-login");
-let loginHide = document.querySelector(".login-hide");
+
+let modalHide = document.querySelectorAll(".modal-times");
 
 for(let i = 0; i < loginButton.length; i++) {
   loginButton[i].onclick = () => {
@@ -9,7 +10,39 @@ for(let i = 0; i < loginButton.length; i++) {
   }
 }
 
-loginHide.onclick = () => {
+modalHide[0].onclick = () => {
   loginForm.classList.add("hidden");
   document.body.style.overflowY = "scroll";
+}
+
+let accountForm = document.querySelector(".account");
+let accountButton = document.querySelectorAll(".header-account");
+
+for(let i = 0; i < accountButton.length; i++) {
+  accountButton[i].onclick = () => {
+    accountForm.classList.remove("hidden");
+    document.body.style.overflowY = "hidden";
+  }
+}
+
+modalHide[1].onclick = () => {
+  accountForm.classList.add("hidden");
+  document.body.style.overflowY = "scroll";
+}
+
+/*=========================*/
+
+let modalFormNext = document.querySelector(".modal__account-form-next");
+let modalFormPrev = document.querySelector(".modal__account-form-prev");
+let modalFormStart = document.querySelector("#modal-form-start");
+let modalFormEnd = document.querySelector("#modal-form-end");
+
+modalFormNext.onclick = () => {
+  modalFormStart.classList.add("hidden");
+  modalFormEnd.classList.remove("hidden");
+}
+
+modalFormPrev.onclick = () => {
+  modalFormStart.classList.remove("hidden");
+  modalFormEnd.classList.add("hidden");
 }
--- projects/longShortCFD/assets/js/select.js
+++ projects/longShortCFD/assets/js/select.js
@@ -1,63 +1,129 @@
-let select = document.querySelectorAll(".hero__account-form .select-input");
-let value = document.querySelectorAll(".hero__account-form .select-input .select-input_value");
-let flag = document.querySelector(".hero__account-form .select-input .select-input_flag img");
-let selection = document.querySelectorAll(".hero__account-form .select-selection");
-let caret = document.querySelectorAll(".select-input .select-caret");
+let codes = ["375", "38", "7", "48"];
+let countries = ["belarus", "ukraine", "russia", "poland"];
 
-for(let i = 0; i < select.length; i++) {
-  select[i].onclick = () => {
-    if(caret[i].classList.contains("open")) {
-      selection.forEach((item, i) => {
+/*=========================*/
+
+let mainSelect = document.querySelectorAll(".hero__account-form .select-input");
+let mainValue = document.querySelectorAll(".hero__account-form .select-input .select-input_value");
+let mainFlag = document.querySelector(".hero__account-form .select-input .select-input_flag img");
+let mainSelection = document.querySelectorAll(".hero__account-form .select-selection");
+let mainCaret = document.querySelectorAll(".modal__account-form .select-input .select-caret");
+
+for(let i = 0; i < mainSelect.length; i++) {
+  mainSelect[i].onclick = () => {
+    if(mainCaret[i].classList.contains("open")) {
+      mainSelection.forEach((item, i) => {
         item.classList.add("hidden");
       });
-      caret[i].classList.remove("open");
-      caret[i].classList.add("close");
-      selection[i].classList.remove("hidden");
-    } else if(caret[i].classList.contains("close")) {
-      selection.forEach((item, i) => {
+      mainCaret[i].classList.remove("open");
+      mainCaret[i].classList.add("close");
+      mainSelection[i].classList.remove("hidden");
+    } else if(mainCaret[i].classList.contains("close")) {
+      mainSelection.forEach((item, i) => {
         item.classList.add("hidden");
       });
-      caret[i].classList.remove("close");
-      caret[i].classList.add("open");
+      mainCaret[i].classList.remove("close");
+      mainCaret[i].classList.add("open");
     }
   }
 
-  let codes = ["375", "38", "7", "48"];
-  let countries = ["belarus", "ukraine", "russia", "poland"];
-
-  select[1].onclick = () => {
-    if(caret[1].classList.contains("open")) {
-      selection.forEach((item, i) => {
+  mainSelect[1].onclick = () => {
+    if(mainCaret[1].classList.contains("open")) {
+      mainSelection.forEach((item, i) => {
         item.classList.add("hidden");
       });
-      caret[1].classList.remove("open");
-      caret[1].classList.add("close");
-      selection[1].classList.remove("hidden");
-    } else if(caret[1].classList.contains("close")) {
-      selection.forEach((item, i) => {
+      mainCaret[1].classList.remove("open");
+      mainCaret[1].classList.add("close");
+      mainSelection[1].classList.remove("hidden");
+    } else if(mainCaret[1].classList.contains("close")) {
+      mainSelection.forEach((item, i) => {
         item.classList.add("hidden");
       });
-      caret[1].classList.remove("close");
-      caret[1].classList.add("open");
+      mainCaret[1].classList.remove("close");
+      mainCaret[1].classList.add("open");
     }
   }
 
-  for(let j = 0; j < (selection[i].children).length; j++) {
-    selection[i].children[j].onclick = () => {
-      value[i].innerHTML = selection[i].children[j].textContent;
+  for(let j = 0; j < (mainSelection[i].children).length; j++) {
+    mainSelection[i].children[j].onclick = () => {
+      mainValue[i].innerHTML = mainSelection[i].children[j].textContent;
     }
-    for(let l = 0; l < (selection[1].children).length; l++) {
-      selection[1].children[l].onclick = () => {
-        value[1].innerHTML = selection[1].children[l].textContent;
-        value[2].innerHTML = "+" + codes[l];
-        flag.src = "../../assets/img/flag/flag_" + countries[l] + ".png";
+    for(let l = 0; l < (mainSelection[1].children).length; l++) {
+      mainSelection[1].children[l].onclick = () => {
+        mainValue[1].innerHTML = mainSelection[1].children[l].textContent;
+        mainValue[2].innerHTML = "+" + codes[l];
+        mainFlag.src = "../../assets/img/flag/flag_" + countries[l] + ".png";
       }
     }
-    for(let k = 0; k < (selection[2].children).length; k++) {
-      selection[2].children[k].onclick = () => {
-        value[2].innerHTML = selection[2].children[k].textContent;
-        value[1].innerHTML = selection[1].children[k].textContent;
-        flag.src = "../../assets/img/flag/flag_" + countries[k] + ".png";
+    for(let k = 0; k < (mainSelection[2].children).length; k++) {
+     mainSelection[2].children[k].onclick = () => {
+        mainValue[2].innerHTML = mainSelection[2].children[k].textContent;
+        mainValue[1].innerHTML = mainSelection[1].children[k].textContent;
+        mainFlag.src = "../../assets/img/flag/flag_" + countries[k] + ".png";
+      }
+    }
+  }
+}
+
+/*=========================*/
+
+let modalSelect = document.querySelectorAll(".modal__account-form .select-input");
+let modalValue = document.querySelectorAll(".modal__account-form .select-input .select-input_value");
+let modalFlag = document.querySelector(".modal__account-form .select-input .select-input_flag img");
+let modalSelection = document.querySelectorAll(".modal__account-form .select-selection");
+let modalCaret = document.querySelectorAll(".modal__account-form .select-input .select-caret");
+
+for(let i = 0; i < modalSelect.length; i++) {
+  modalSelect[i].onclick = () => {
+    if(modalCaret[i].classList.contains("open")) {
+      modalSelection.forEach((item, i) => {
+        item.classList.add("hidden");
+      });
+      modalCaret[i].classList.remove("open");
+      modalCaret[i].classList.add("close");
+      modalSelection[i].classList.remove("hidden");
+    } else if(mainCaret[i].classList.contains("close")) {
+      modalSelection.forEach((item, i) => {
+        item.classList.add("hidden");
+      });
+      modalCaret[i].classList.remove("close");
+      modalCaret[i].classList.add("open");
+    }
+  }
+
+  modalSelect[1].onclick = () => {
+    if(modalCaret[1].classList.contains("open")) {
+      modalSelection.forEach((item, i) => {
+        item.classList.add("hidden");
+      });
+      modalCaret[1].classList.remove("open");
+      modalCaret[1].classList.add("close");
+      modalSelection[1].classList.remove("hidden");
+    } else if(modalCaret[1].classList.contains("close")) {
+      modalSelection.forEach((item, i) => {
+        item.classList.add("hidden");
+      });
+      modalCaret[1].classList.remove("close");
+      modalCaret[1].classList.add("open");
+    }
+  }
+
+  for(let j = 0; j < (modalSelection[i].children).length; j++) {
+   modalSelection[i].children[j].onclick = () => {
+      modalValue[i].innerHTML = modalSelection[i].children[j].textContent;
+    }
+    for(let l = 0; l < (modalSelection[1].children).length; l++) {
+     modalSelection[1].children[l].onclick = () => {
+        modalValue[1].innerHTML = modalSelection[1].children[l].textContent;
+        modalValue[2].innerHTML = "+" + codes[l];
+        modalFlag.src = "../../assets/img/flag/flag_" + countries[l] + ".png";
+      }
+    }
+    for(let k = 0; k < modalSelection[2].children.length; k++) {
+     modalSelection[2].children[k].onclick = () => {
+        modalValue[2].innerHTML = modalSelection[2].children[k].textContent;
+        modalValue[1].innerHTML = modalSelection[1].children[k].textContent;
+        modalFlag.src = "../../assets/img/flag/flag_" + countries[k] + ".png";
       }
     }
   }
