930636d1f74d6ce3911c2e699a7098f194b2efcb
Anton Miazyn <antonmiazyn@mail.ru>
2020-11-06 01:13:59 +0200

    Main page beta v2

--- projects/hermes/assets/js/menu-navigation.js
+++ projects/hermes/assets/js/menu-navigation.js
@@ -1,10 +1,11 @@
 let toggleButton = document.querySelector(".toggle");
 let menuBlock = document.querySelector(".main-navigation");
+let timesButton = menuBlock.querySelector(".times");
 
 toggleButton.onclick = () => {
-  if(!menuBlock.classList.contains("shown")) {
-    menuBlock.classList.add("shown");
-  } else {
-    menuBlock.classList.remove("shown");
-  }
+  menuBlock.classList.add("shown");
+}
+
+timesButton.onclick = () => {
+  menuBlock.classList.remove("shown");
 }
