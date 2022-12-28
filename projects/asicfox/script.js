79de288e7f000c77c8c403d681889ed5191f5168
Anton Miazyn <miazyn.anton@gmail.com>
2022-12-28 21:24:48 +0200

    Add files via upload

--- projects/asicfox/script.js
+++ projects/asicfox/script.js
@@ -133,3 +133,100 @@ function getDHMS (ms) {
 
   return { days, hours, minutes, seconds }
 }
+
+// Animation: Snow
+
+var Snowflake = (function() {
+
+	var flakes;
+	var flakesTotal = 120;
+	var wind = 0;
+	var mouseX;
+	var mouseY;
+
+	function Snowflake(size, x, y, vx, vy) {
+		this.size = size;
+		this.x = x;
+		this.y = y;
+		this.vx = vx;
+		this.vy = vy;
+		this.hit = false;
+		this.melt = false;
+		this.div = document.createElement('div');
+		this.div.classList.add('landing--snowflake');
+		this.div.style.width = this.size + 'px';
+		this.div.style.height = this.size + 'px';
+	}
+
+	Snowflake.prototype.move = function() {
+		if (this.hit) {
+			if (Math.random() > 0.995) this.melt = true;
+		} else {
+			this.x += this.vx + Math.min(Math.max(wind, -10), 10);
+			this.y += this.vy;
+		}
+
+		// Wrap the snowflake to within the bounds of the page
+		if (this.x > window.innerWidth + this.size) {
+			this.x -= window.innerWidth + this.size;
+		}
+
+		if (this.x < -this.size) {
+			this.x += window.innerWidth + this.size;
+		}
+
+		if (this.y > window.innerHeight + this.size) {
+			this.x = Math.random() * window.innerWidth;
+			this.y -= window.innerHeight + this.size * 2;
+			this.melt = false;
+		}
+
+		var dx = mouseX - this.x;
+		var dy = mouseY - this.y;
+		this.hit = !this.melt && this.y < mouseY && dx * dx + dy * dy < 2400;
+	};
+
+	Snowflake.prototype.draw = function() {
+		this.div.style.transform =
+		this.div.style.MozTransform =
+		this.div.style.webkitTransform =
+			'translate3d(' + this.x + 'px' + ',' + this.y + 'px,0)';
+	};
+
+	function update() {
+		for (var i = flakes.length; i--; ) {
+			var flake = flakes[i];
+			flake.move();
+			flake.draw();
+		}
+		requestAnimationFrame(update);
+	}
+
+	Snowflake.init = function(container) {
+		flakes = [];
+
+		for (var i = flakesTotal; i--; ) {
+			var size = (Math.random() + 0.2) * 12 + 1;
+			var flake = new Snowflake(
+				size,
+				Math.random() * window.innerWidth,
+				Math.random() * window.innerHeight,
+				Math.random() - 0.5,
+				size * 0.2
+			);
+			container.appendChild(flake.div);
+			flakes.push(flake);
+		}
+    
+  	update();
+	};
+
+	return Snowflake;
+
+}());
+
+window.onload = function() {
+  setTimeout(function() {
+  	Snowflake.init(document.getElementById('landing--snow'));
+  }, 500);
+}
